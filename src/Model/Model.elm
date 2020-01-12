62f36f4e51acd47c68cb2da3b630cd4dd70ec479
Marta Kowalczyk <mk2543@gmail.com>
2020-01-12 18:23:51 +0100

    Full model

--- src/Model/Model.elm
+++ src/Model/Model.elm
@@ -4,12 +4,36 @@ type alias Model =
     {
         gitlabUrl: String
         , mergeRequests: List MergeRequest
+        , filters: Filters
     }
+
+type PipelineStatus = Passed | Failed | Warning   
+type App = Frontend | Backend 
+
+type alias MrId = String
 
 type alias MergeRequest = 
   {
-    id: String
+    id: MrId
+    , appType: App
     , name: String
+    , author: String
+    , openSince: String
+    , approvals: Int
+    , approvedByMe: Bool
+    , isWip: Bool
+    , pipelinePassed: PipelineStatus
+    , changedFiles: Int
   }
+
+type alias Filters = {
+    approvedMrsVisible: Bool
+    , failedPipelinesVisible: Bool
+    , wipsVisible: Bool
+  }  
+
+type FilterMessages = HideApprovedMrs | ShowApprovedMrs | HideFailedPipelines 
+  | ShowFailedPipelines | HideWips | ShowWips | ClearFilters
+
 type Msg
-    = NoOp    
+    = Merge MrId | FilterMessages    
